3383e50913828c31cf4ee655408b2a73fb02ea3d
Jonathan A. Mitchell <ontoneio.mm@gmail.com>
2020-10-29 14:49:25 -0700

    add head tag configuration

--- _site/nuxt.config.js
+++ _site/nuxt.config.js
@@ -5,19 +5,25 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
 
+
+  // <link type="text/css" rel="stylesheet" href="/ece/main.css" />
+	// <script src="/acgov.js" type="text/javascript"></script>
+	// <script src="/ece/main.js" type="text/javascript"></script>
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'site',
+    title: 'Early Care & Education Planning Council - Alameda County',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Alameda County General Services Agency Childcare Home Page' },
+      { name: 'keywords', content: 'Alameda County Early Care & Education Planning Council, Council, Planning, Childhood, Early Childhood,  Needs, School, Schools, Proposition 10, Subsidized, Kids, Children, Commission, Community, Child, Child Care, Partnership, Ala-ASCC, Alameda Access to Subsidized Child Care, LINCC, Local Investment in Child Care, UASCC, Universal Access to Subsidized Child Care' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: [
+    '~/static/css/main.css'
+  ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   // plugins: [],
