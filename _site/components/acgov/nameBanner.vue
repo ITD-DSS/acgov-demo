5b8805c2db199e7c069011876016b5cc89401cad
Jonathan A. Mitchell <ontoneio.mm@gmail.com>
2020-11-09 19:12:21 -0800

    styles with tailwind

--- _site/components/acgov/nameBanner.vue
+++ _site/components/acgov/nameBanner.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="container-fluid branding">
-    <div class="row container resp">
-      <div class="col-sm-6 col-xs-12 logo">
-        <img class="img-responsive acgov-logo" src="/img/brandingPROD.png" />
+  <div v-once class="fixed w-full h-20 shadow-banner bg-white z-10">
+    <!-- <div class="container-fluid branding"> -->
+    <div class="flex flex-row justify-around items-center pt-3">
+      <!-- <div class="row container resp"> -->
+
+      <!-- <div class="col-sm-6 col-xs-12 logo"> -->
+      <div class="mr-8">
+        <img class="w-logo" src="/img/brandingPROD.png" />
       </div>
-      <div class="col-sm-6 col-xs-12 tagline">County of Alameda, CA</div>
+      <!-- <img class="img-responsive acgov-logo" src="/img/brandingPROD.png" /> -->
+
+      <div class="ml-5 text-banner">County of Alameda, CA</div>
+      <!-- <div class="col-sm-6 col-xs-12 tagline">County of Alameda, CA</div> -->
     </div>
   </div>
 </template>
@@ -15,4 +22,22 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+/*
+-webkit-tap-highlight-color: rgba(0,0,0,0);
+font-size: 14px;
+line-height: 1.42857143;
+color: #333;
+font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+box-sizing: border-box;
+background-color: #fff;
+margin: 0px;
+padding: 0px;
+opacity: 1;
+position: fixed;
+top: 0px;
+width: 100%;
+z-index: 10;
+box-shadow: #000 0px 2px 15px;
+*/
+</style>
