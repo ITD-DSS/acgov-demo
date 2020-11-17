245e4e79f8567afb828acbe3e456e308e910c47c
Jonathan A. Mitchell <ontoneio.mm@gmail.com>
2020-11-17 15:08:04 -0800

    new components

---
+++ _site/components/acgov/stories/govDeliveryLink.vue
@@ -0,0 +1,21 @@
+<template>
+  <a
+    data-toggle="modal"
+    class="w-32 self-end absolute"
+    data-target="#modal-govd"
+  >
+    <img
+      class=""
+      src="/img/get-email-updates.png"
+      alt="GovDelivery icon with text saying Get Email Updates"
+    />
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'GovDeliveryLink',
+}
+</script>
+
+<style></style>
