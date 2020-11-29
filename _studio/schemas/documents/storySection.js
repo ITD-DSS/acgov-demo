1d74a650997218bf908e30c03485832adc190ae3
Jonathan A. Mitchell <ontoneio.mm@gmail.com>
2020-11-28 23:05:45 -0800

    adds slug field

--- _studio/schemas/documents/storySection.js
+++ _studio/schemas/documents/storySection.js
@@ -14,6 +14,17 @@ export default {
       type: "string",
     },
     {
+      title: "Section Slug",
+      name: "sectionSlug",
+      type: "slug",
+      options: {
+        source: (doc) => `${doc.sectionName}`,
+        maxLength: 25, // will be ignored if slugify is set
+        slugify: (input) =>
+          input.toLowerCase().replace(/\s+/g, "-").slice(0, 25),
+      },
+    },
+    {
       title: "Section Stories",
       name: "sectionStories",
       type: "reference",
