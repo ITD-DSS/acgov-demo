76c0b020c79f9453f4bc049464eb9f17babbffff
Jonathan A. Mitchell <ontoneio.mm@gmail.com>
2020-12-04 17:05:44 -0800

    remove unused property

--- _site/components/acgov/stories/imgLink.vue
+++ _site/components/acgov/stories/imgLink.vue
@@ -1,13 +1,13 @@
 <template>
   <a v-if="linkTo" target="_blank" :href="linkTo">
-    <SanityImage :asset-id="imgSrc" auto="format">
+    <SanityImage :asset-id="imgSrc">
       <!-- <template #default="{ src }"> -->
       <!-- <img class="w-full my-2" :src="src" :alt="imgAlt" /> -->
       <!-- <img :src="src" /> -->
       <!-- </template> -->
     </SanityImage>
   </a>
-  <SanityImage v-else :asset-id="imgSrc" auto="format">
+  <SanityImage v-else :asset-id="imgSrc">
     <!-- <template #default="{ src }"> -->
     <!-- <img class="w-full my-2" :src="src" :alt="imgAlt" /> -->
     <!-- <img :src="src" /> -->
