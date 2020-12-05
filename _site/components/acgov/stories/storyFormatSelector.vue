331017171a6a4cc578dc1131fae5fa3d4a5f26cf
Jonathan A. Mitchell <ontoneio.mm@gmail.com>
2020-12-04 17:58:39 -0800

    clean up

--- _site/components/acgov/stories/storyFormatSelector.vue
+++ _site/components/acgov/stories/storyFormatSelector.vue
@@ -31,44 +31,13 @@
 </template>
 
 <script>
-// const storyPropValidator = (propValue) => {
-//   const keys = Object.keys(propValue)
-
-//   const hasDefaults = ['_key', '_type'].includes(...keys)
-//   const isNews = ['headline', 'storyBody'].includes(...keys)
-//   debugger
-//   const isImage = ['linkImage', 'linkTo'].includes(...keys)
-//   const isVideo = ['altText', 'youtubeUrl'].includes(...keys)
-
-//   const oneValid = [isNews, isImage, isVideo].find((format) => format === true)
-
-//   if (!hasDefaults) {
-//     return false
-//   }
-
-//   if (oneValid) {
-//     return true
-//   }
-
-//   return false
-// }
-
 export default {
   name: 'StoryFormatSelector',
   props: {
-    // categoryTag: {
-    //   type: String,
-    //   default: '',
-    // },
     componentData: {
       type: Object,
       default: () => {},
-      //   validator: (prop) => storyPropValidator(prop),
     },
-    // storyLayout: {
-    //   type: String,
-    //   default: '',
-    // },
   },
   data() {
     return {
@@ -89,56 +58,21 @@ export default {
         return (this.format = this.componentData.format)
       }
     },
-
-    // parseFormats() {
-    //   const formatType = this.getStoryFormat
-    //   switch (formatType) {
-    //     case 'textStory':
-    //       this.newsStory.category = this.formatData.storyTag
-    //       this.newsStory.keyId = this.formatData._key
-    //       this.newsStory.headline = this.formatData.headline
-    //       this.newsStory.body = this.formatData.storyBody
-    //       break
-    //     // case 'imageLink':
-    //     //   if (this.formatData.linkTo !== undefined) {
-    //     //     this.imageLink.destLink = this.formatData.linkTo
-    //     //     this.imageLink.alt = this.formatData.linkImage.altText
-    //     //     this.imageLink.src = this.formatData.linkImage.asset._ref
-    //     //     this.imageLink.keyId = this.formatData._key
-    //     //   }
-    //     //   this.imageLink.alt = this.formatData.linkImage.altText
-    //     //   this.imageLink.src = this.formatData.linkImage.asset._ref
-    //     //   this.imageLink.keyId = this.formatData._key
-    //     //   break
-    //     case 'videoStory':
-    //       this.video.url = this.formatData.youtubeUrl
-    //       this.video.alt = this.formatData.altText
-    //       this.video.keyId = this.formatData._key
-    //       break
-    //     default:
-    //       break
-    //   }
-    // },
   },
 }
 </script>
 
 <style>
 .full {
-  /* grid-column: span 1 / span 3; */
   grid-column-start: 1;
   grid-column-end: 3;
   margin-bottom: 1.75rem;
 }
 .left {
-  /* grid-column: span 1 / span 2; */
-  /* grid-column-end: 2; */
   grid-column-start: 1;
   grid-column-end: 2;
 }
 .right {
-  /* grid-column: span 2 / span 3; */
-  /* grid-column-end: 2; */
   grid-column-start: 2;
   grid-column-end: 3;
 }
